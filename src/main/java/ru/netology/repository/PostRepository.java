b32b57166c5899e2c7ec2067e1d6ddae1ffb203c
queen <Queenmoncler@gmail.com>
2023-10-19 20:01:04 +0300

    di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
@@ -9,6 +10,7 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
+@Repository
 public class PostRepository {
     private final ConcurrentMap<Long, Post> allPosts;
     private final AtomicLong idCounter = new AtomicLong();
